b6ebdaa09efae536c256708159c8e142d25ec2b1
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-11-04 12:53:56 +0100

    Use vsntool

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hukai.MixProject do
   def project do
     [
       app: :hukai,
-      version: "0.1.0",
+      version: File.read!("VERSION"),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
